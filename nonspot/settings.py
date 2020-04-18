508558b68a2f00ace751e0592dc7aba5d2fc2482
mrubio-chavarria <msrch@hotmail.es>
2020-04-18 11:50:22 +0200

    Fix database conf

--- nonspot/settings.py
+++ nonspot/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'nonspot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -105,7 +105,7 @@ DATABASES = {
         'PORT':  5432
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
