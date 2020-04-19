581cf48ff3f7c0927ed240aa4193db8c9faffc13
mrubio-chavarria <msrch@hotmail.es>
2020-04-20 00:34:31 +0200

    Add name of app admin

--- services/evaluations/urls.py
+++ services/evaluations/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from services.evaluations import views as evaluations
+from django.contrib import admin
 
 router = routers.SimpleRouter()
 router.register('evaluations', evaluations.EvaluationViewSet, base_name='evaluations')
@@ -8,3 +9,7 @@ router.register('evaluations', evaluations.EvaluationViewSet, base_name='evaluat
 urlpatterns = [
     url(r'^', include(router.urls)),
 ]
+
+admin.site.site_header = 'Administración de nonSpot'
+admin.site.site_title = 'Administración de nonSpot'
+
